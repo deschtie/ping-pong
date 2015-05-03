c69ee6c2777f66b4d9f8d520183e5df59b0c18f6
Desire <dperraultreid@gmail.com>
2015-05-03 16:52:37 -0700

    4 of 4 specs pass

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,11 +1,19 @@
 class Fixnum
 
-  define_method(:list_numbers) do
+  define_method(:ping_pong) do
   ping_pong_array = []
-    self.times() do |time|
-      start = time.+(1)
-      ping_pong_array.push(start)
+    times() do |time|
+      number = time.+(1)
+      if number.%(3).eql?(0).&(number.%(5).eql?(0))
+        ping_pong_array.push("ping-pong")
+      elsif number.%(3).eql?(0)
+        ping_pong_array.push("ping")
+      elsif number.%(5).eql?(0)
+        ping_pong_array.push("pong")
+      else
+      ping_pong_array.push(number)
+      end
     end
-  ping_pong_array
+    ping_pong_array
   end
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,13 +1,20 @@
 require('rspec')
 require('ping_pong')
 
-describe(Fixnum)do
+describe(Fixnum) do
 
-  describe("#list_numbers") do
+  describe("#ping_pong") do
     it("takes the number passed through the method, and returns an array starting at 1 and ending with that number") do
-    expect(6.list_numbers()).to(eq([1, 2, 3, 4, 5, 6]))
+      expect(2.ping_pong()).to(eq([1, 2]))
+    end
+   it("returns ping instead of a number if that number is a multiple of 3") do
+      expect(4.ping_pong()).to(eq([1, 2, "ping", 4]))
+    end
+     it("returns pong instead of a number if that number is a multiple of 5") do
+       expect(9.ping_pong()).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping"]))
+    end
+    it("returns ping-pong instead of the number if that number is a multiple of both 3 and 5") do
+      expect(15.ping_pong()).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]))
     end
   end
-
-
 end
